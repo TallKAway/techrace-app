3fd9d6f74633219fa8f46a822170deab8184725e
Léa Mhoumadi <95865130+Leambr@users.noreply.github.com>
2024-07-01 10:57:54 +0200

    Reset colors and files (#11)
    * chore: update expo versions
    
    * feat: create color theme and delete unused files

---
+++ styles/ColorTheme.ts
@@ -0,0 +1,13 @@
+import Colors from './constants/Colors';
+
+export const ColorTheme = {
+    dark: false,
+    colors: {
+        primary: Colors.primary,
+        background: Colors.background,
+        card: Colors.white,
+        text: Colors.text,
+        border: Colors.border,
+        notification: Colors.accent,
+    },
+};
