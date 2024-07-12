2737fd5c8aaf608e1fb873ee13fda811187a4174
mat <112476420+matteoLED@users.noreply.github.com>
2024-07-12 18:26:17 +0200

    add data from websocket

--- shared/providers/SocketContext.tsx
+++ shared/providers/SocketContext.tsx
@@ -2,23 +2,51 @@ import React, { createContext, useContext, useEffect, useState, ReactNode } from
 
 interface WebSocketContextProps {
     socket: WebSocket | null;
+    speed: number | null;
+    battery: number | null;
+    video_url: string | null;
 }
+const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL;
 
-const SOCKET_URL = process.env.EXPO_PUBLIC_SOCKET_URL || '';
-
-const WebSocketContext = createContext<WebSocketContextProps>({ socket: null });
+const WebSocketContext = createContext<WebSocketContextProps>({
+    socket: null,
+    speed: null,
+    battery: null,
+    video_url: null,
+});
 
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
 
+    const [speed, setSpeed] = useState<number | null>(null);
+    const [battery, setBattery] = useState<number | null>(null);
+    const [video_url, setVideoUrl] = useState<string | null>(null);
+
     useEffect(() => {
-        if (SOCKET_URL !== '') {
+        if (SOCKET_URL) {
             const newSocket = new WebSocket(SOCKET_URL);
 
             console.log();
 
-            newSocket.onopen = () => {
-                console.log('WebSocket connection established');
+            newSocket.onclose = () => {
+                console.log('WebSocket connection closed');
+            };
+
+            newSocket.onmessage = (message) => {
+                console.log('WebSocket message received:', message.data);
+                try {
+                    const data = JSON.parse(message.data);
+
+                    if (data.speed !== undefined && data.battery !== undefined) {
+                        setSpeed(Math.round(data.speed));
+                        setBattery(data.battery);
+                        setVideoUrl(data.videoUrl);
+                    } else {
+                        console.error('Invalid data format:', data);
+                    }
+                } catch (error) {
+                    console.error('Failed to parse message data:', error);
+                }
             };
 
             newSocket.onclose = () => {
@@ -39,7 +67,11 @@ export const SocketProvider = ({ children }: { children: ReactNode }) => {
         }
     }, []);
 
-    return <WebSocketContext.Provider value={{ socket }}>{children}</WebSocketContext.Provider>;
+    return (
+        <WebSocketContext.Provider value={{ socket, speed, battery, video_url }}>
+            {children}
+        </WebSocketContext.Provider>
+    );
 };
 
 export const useSocket = () => {
