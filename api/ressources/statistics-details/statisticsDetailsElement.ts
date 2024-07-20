a56e608580b0d15b7da8c0a642e0d12d6dc7bf22
Louis <roccalouis26@gmail.com>
2024-07-20 21:46:37 +0200

    feat: use techRaceApiCall to in statisticDetailsElement call api

--- api/ressources/statistics-details/statisticsDetailsElement.ts
+++ api/ressources/statistics-details/statisticsDetailsElement.ts
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
+import { techRaceApiCall } from '../utils/api';
+
 import { StatisticsDetailsElementsResponse } from './types';
 
 export const useGetStatisticsDetailsElements = (id: number) => {
@@ -8,25 +10,11 @@ export const useGetStatisticsDetailsElements = (id: number) => {
 
     return useQuery<StatisticsDetailsElementsResponse>({
         queryKey: ['statisticsDetails', id, API_KEY, API_URL],
+
         queryFn: () =>
-            fetch(`${API_URL}/race/${id}`, {
+            techRaceApiCall(`/race/${id}`, {
                 body: null,
                 method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-api-key': `${API_KEY}`,
-                },
-            }).then((response) => {
-                if (!response.ok) {
-                    throw {
-                        response: response,
-                        error: new Error(
-                            `Error: ${response.url} ${response.status} ${response.statusText}`
-                        ),
-                    };
-                }
-
-                return response.json();
             }),
     });
 };
