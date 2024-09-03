9ce425245c2081d2c928201d4066b15f70656993
Louis <roccalouis26@gmail.com>
2024-09-03 17:16:50 +0200

    test(wip): trying to test statistics details screen with testing library

---
+++ api/ressources/statistics-details/statisticsDetailsElement.fixture.ts
@@ -0,0 +1,14 @@
+export const getStatisticsDetailsElementsApiResponse = {
+    data: {
+        data: {
+            battery: [],
+            distance: 300,
+            end_Time: '2024-07-05T15:59:48.000Z',
+            id: 10,
+            speeds: [[Object]],
+            start_Time: '2024-07-05T15:44:48.000Z',
+            timestamp: '2024-07-05T15:44:48.000Z',
+        },
+        status: 'success',
+    },
+};
